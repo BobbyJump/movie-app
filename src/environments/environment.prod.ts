a14f0a9d845238f94382a2e96a5d278cf8baaf3d
Dmytro <skachok.d@gmail.com>
2022-11-18 17:13:48 +0200

    added firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
-import { environment as common } from './environment';
-
 export const environment = {
-  ...common,
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBtQdwwgJavneoEJCCJ_6Nyq_MZ7hcP7Wo",
+    authDomain: "kursova-78ff9.firebaseapp.com",
+    projectId: "kursova-78ff9",
+    storageBucket: "kursova-78ff9.appspot.com",
+    messagingSenderId: "1044785505189",
+    appId: "1:1044785505189:web:ee801a283b0dd2378c3e4c",
+    measurementId: "G-RQXD8JJCYV"
+  }
 };
